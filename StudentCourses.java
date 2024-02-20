f3d3404a791e69be1fdabb2cd90e6f5c5ff60444
unknown <Administrator@Newcaed20.MDServer.bvb.edu>
2024-02-20 10:46:13 +0530

    courses and marks displayed

---
+++ StudentCourses.java
@@ -0,0 +1,14 @@
+public class StudentCourses {
+
+    private String[] courseCodes = { "C#", "ST", "ML", "DEVOPS", "MP" };
+    private int[] marks = { 85, 92, 75, 88, 95 };
+
+    public void displayStudentMarks(String course) {
+        System.out.println("Course Codes \t Marks Obtained");
+
+        for (int i = 0; i < courseCodes.length; i++) {
+            if (course.equalsIgnoreCase(courseCodes[i]))
+                System.out.println(courseCodes[i] + "\t\t\t" + marks[i]);
+        }
+    }
+}
